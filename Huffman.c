9399acc8615978a1c1bb30cfb8ff67ec258804d3
patriani <43487367+patriani@users.noreply.github.com>
2019-11-27 22:18:16 -0300

    Update Huffman.c

--- Huffman.c
+++ Huffman.c
@@ -35,16 +35,13 @@ void ordenaMatr(int** elem){
             }
 }
 
-LISTA* initLista(){
-    LISTA* lista;
-    lista = (LISTA*)malloc(sizeof(LISTA));
-    lista->info = NULL;
-    lista->prox = NULL;
-    return lista;
+void initLista(LISTA** lista){
+    (*lista) = NULL;
 }
 
 LISTA* novoLista(){ // novo nó na lista
-    LISTA* lista = initLista();
+    LISTA* lista = NULL;
+    lista = (LISTA*)malloc(sizeof(LISTA));
     lista->info = NULL;
     lista->prox = NULL;
     return lista;
@@ -53,7 +50,7 @@ LISTA* novoLista(){ // novo nó na lista
 LISTA* busca(LISTA* ptr){
     LISTA* aux = (LISTA*)malloc(sizeof(LISTA));
     aux = ptr;
-    if(ptr -> prox == NULL)
+    if(ptr == NULL || ptr->prox == NULL)
         return ptr;
     while(aux->prox != NULL){
         aux = (LISTA*) (aux -> prox);
@@ -61,23 +58,47 @@ LISTA* busca(LISTA* ptr){
     return aux;
 }
 
-void addLista(LISTA* ptr, NO* novo){ // cria nó na lista com um elemento da futura árvore
-    LISTA* aux = busca(ptr);
-    LISTA* lista = novoLista();
+/*
+void addLista(LISTA** ptr, NO* novo){ // cria nó na lista com um elemento da futura árvore
+    LISTA* aux = busca((*ptr)); // aux recebe endereço do último elemento da lista
+    LISTA* lista = novoLista(); // um nó de lista
     aux->prox = (struct sLISTA*) lista; // adiciona nó na lista
     lista->info = novo; // nó da lista recebe nó da futura árvore
 }
 
-void novoNo(LISTA* ptr,char info, int freq){
+void novoNo(LISTA** ptr,char info, int freq){
     NO* novo;
     novo = (NO*) malloc(sizeof(NO));
-    novo->infoCh = info;
+    novo = NULL;
     novo->infoNum = freq;
-    novo->dir = NULL;
-    novo->esq = NULL;
+    novo->infoCh = info;
     addLista(ptr,novo);
 
+}*/
+
+
+
+NO* novoNo(char info, int freq){
+    NO* novo;
+    novo = NULL;
+    novo = (NO*) malloc(sizeof(NO));
+    novo->infoNum = freq;
+    novo->infoCh = info;
+    return novo;
 }
+
+void addLista(LISTA** ptr, char info, int freq){ // cria nó na lista com um elemento da futura árvore
+    LISTA* aux = busca((*ptr)); // aux recebe endereço do último elemento da lista
+    LISTA* lista = novoLista(); // um nó de lista
+    NO* novo = novoNo(info,freq);
+    lista->info = novo; // nó da lista recebe nó da futura árvore
+    if((*ptr) == NULL )
+        (*ptr) =  lista; 
+    else
+        aux->prox = (struct sLISTA*) lista; // adiciona nó na lista
+}
+
+
 
 int swt(int aux){ // switch para atualizar frequência de char
 
@@ -101,23 +122,23 @@ NO* novoArvore(int freq){
     return novo;
 }
 
-void limpaLista(LISTA* ptr){ // apaga os campos da lista que não estão sendo utilizados (os dois primeiros elementos )
-    LISTA* aux = ptr;
+void limpaLista(LISTA** ptr){ // apaga os campos da lista que não estão sendo utilizados (os dois primeiros elementos )
+    LISTA* aux = (*ptr);
     aux =(LISTA*) aux->prox;
     aux =(LISTA*) aux->prox;
-    ptr = aux;
-}
+    (*ptr) = aux;
+} 
 
-void insereNo(LISTA* ptr,NO* novo){ // insere a raiz na lista de forma ordenada
+void insereNo(LISTA** ptr,NO* novo){ // insere a raiz na lista de forma ordenada
     int i = 0; //recebe 1 quando o nó foi inserido
-    LISTA* aux1 =(LISTA*) ptr->prox; // auxiliar para percorrer lista
-    LISTA* aux2 = ptr;
+    LISTA* aux1 =(LISTA*) (*ptr)->prox; // auxiliar para percorrer lista
+    LISTA* aux2 = (*ptr);
     LISTA* novo_lista = novoLista();
     novo_lista->info = novo;
     
-    if((ptr->info)->infoNum > novo->infoNum){ // insere no início
-            novo_lista->prox = ptr->prox;
-            ptr = novo_lista;
+    if(((*ptr)->info)->infoNum > novo->infoNum){ // insere no início
+            novo_lista->prox = (*ptr)->prox;
+            (*ptr) = novo_lista;
             i = 1;
         }
     
@@ -127,65 +148,81 @@ void insereNo(LISTA* ptr,NO* novo){ // insere a raiz na lista de forma ordenada
             aux1 =(LISTA*) aux1->prox;
             aux2 =(LISTA*) aux2->prox;
         }else if((aux1->info)->infoNum > novo->infoNum){ // insere no meio
-            aux2->prox =(LISTA*) novo_lista;
-            novo_lista->prox =(LISTA*) aux1;
-            i = 1;
+            aux2->prox = (struct sLISTA*)novo_lista;
+            novo_lista->prox =(struct sLISTA*) aux1;
+            i = 1; // condição de saída
         }else{ // insere no fim
-            aux1->prox = novo_lista;
+            aux1->prox =(struct sLISTA*) novo_lista;
             novo_lista->prox = NULL;
-            i = 1;
+            i = 1; // condição de saída
         }
     }
     }
 }
 
-void passoHuffman(LISTA* ptr){ 
+
+void passoHuffman(LISTA** ptr){ 
     int freq = 0;
-    LISTA* aux1 = ptr;
-    LISTA* aux2 =(LISTA*) ptr->prox;
+    int freq1 = 0,freq2 = 0;
+    LISTA* aux1 = NULL;
+    aux1 = (*ptr);
+    LISTA* aux2 = NULL;
+    aux2 = (LISTA*) (*ptr)->prox;
+    NO* temp = NULL;
 
-    while(ptr->prox != NULL){
+    if((*ptr) != NULL){
+        //printf("%d",(aux1->info)->infoNum);
+        while((*ptr)->prox != NULL){
+            //printf("\n\n\nentrou no while");
+            
+            temp = (aux1->info);
+            freq1 = temp->infoNum;
+            temp = (aux2->info);
+            freq2 = (temp->infoNum);
+            freq =  freq1 + freq2; // frequência total da nova raiz 
+            temp = NULL;
 
-        freq = ((aux1->info)->infoNum) + ((aux2->info)->infoNum); // frequência total da nova raiz 
+            NO* novo = novoArvore(freq); // cria novo nó
 
-        NO* novo = novoArvore(freq); // cria novo nó
-
-        if((aux1->info)->infoCh == '*' && (aux2->info)->infoCh != '*'){ //campo char do elemento com valor "*" representa uma raiz
-            novo->infoCh = '*';
-            novo->dir = aux2;
-            novo->esq = aux1;        
-        }else if((aux1->info)->infoCh != '*' && (aux2->info)->infoCh == '*'){
-            novo->infoCh = '*';
-            novo->dir = aux1;
-            novo->esq = aux2;         
-        }else if((aux1->info)->infoCh == '*' && (aux2->info)->infoCh == '*' || (aux1->info)->infoCh != '*' && (aux2->info)->infoCh != '*'){ 
-            if((aux1->info)->infoNum > (aux2->info)->infoNum ){
+            if((aux1->info)->infoCh == '*' && (aux2->info)->infoCh != '*'){ //campo char do elemento com valor "*" representa uma raiz
                 novo->infoCh = '*';
-                novo->dir = aux1;
-                novo->esq = aux2;
-            }else{
+                novo->dir = aux2->info;
+                novo->esq = aux1->info;        
+            }else if((aux1->info)->infoCh != '*' && (aux2->info)->infoCh == '*'){
                 novo->infoCh = '*';
-                novo->dir = aux2;
-                novo->esq = aux1;
+                novo->dir = aux1->info;
+                novo->esq = aux2->info;         
+            }else if((aux1->info)->infoCh == '*' && (aux2->info)->infoCh == '*' || (aux1->info)->infoCh != '*' && (aux2->info)->infoCh != '*'){ 
+                if((aux1->info)->infoNum > (aux2->info)->infoNum ){
+                    novo->infoCh = '*';
+                    novo->dir = aux1->info;
+                    novo->esq = aux2->info;
+                }else{
+                    novo->infoCh = '*';
+                    novo->dir = aux2->info;
+                    novo->esq = aux1->info;
+                }
             }
+
+            limpaLista(ptr);
+
+            insereNo(ptr,novo);
         }
-
-        limpaLista(ptr);
-
-        insereNo(ptr,novo);
     }
-    
 }
 
 int main(){
     FILE* fileR; // path para leitura do arquivo
     FILE* fileW; // paths para escrita 
     int vetInicial[53][2]; //guarda as frequencias das letras em [x][0] e qual o char em [x][1]
-    LISTA* ptr = initLista(); // aponta para raiz (inicializado com NULL)
+    LISTA* ptr ; // aponta para raiz (inicializado com NULL)
     int aux = 0,i = 0, j = 0, aux2 = 0, menor = 600; // 600 é apenas um valor arbitrário e alto para "menor"
+    int temp = 0;
 
-    fileR = fopen("entrada.txt","r");
-    //fileW = fopen("entrada.txt","w");
+    initLista(&ptr);
+
+    fileR = fopen("entrada.txt","r"); //abre arquivo para leitura
+    fileW = fopen("saida.dvz","a"); // abre arquivo para escrita
     
     printf("0 - compactar / 1 - descompactar: ");
     scanf("%d",&aux);
@@ -213,8 +250,6 @@ int main(){
             j++;
             }while(j != 53);
 
-            int temp = 0;
-
             do{ // lê arquivo e conta frequencia
                 
                 aux = fgetc(fileR); //leitura char por char do arquivo
@@ -232,31 +267,35 @@ int main(){
 
             j = 0;
 
-            while(i < 53){
+            while(i < 53){ // adiciona nós na lista encadeada com as raízes 
                 int temp = vetInicial[j][0];
                     if(temp != 0){
                         if(menor > temp){
                             menor = temp;
-                            vetInicial[j][0] = 0; // zera a frequência e não eh mais considerado na proxima passagem pela matriz
                             aux2 = j;
                         }
                         if(j == 52){ // chegou à última posição
-                            i = 0;
-                            j = 0; // reinicia o contador para percorrer a matriz
-                            novoNo(ptr,vetInicial[aux2][1],vetInicial[aux2][0]);
+                            i = 0; // o contador de zeros reinicia para não contar novamente os elementos de frequência nula da leitura anterior
+                            char tempCh = vetInicial[aux2][1];
+                            //printf("%d , %c", tempCh);
+                            //novoNo(&ptr,tempCh,vetInicial[aux2][0]);           
+                            addLista(&ptr,tempCh,vetInicial[aux2][0]);
+                            vetInicial[j][0] = 0; // zera a frequência e não eh mais considerado na proxima passagem pela matriz
                             aux2 = 0;
+                            j = 0; // reinicia o contador para percorrer a matriz
                             menor = 600; // reiniciando 
                         }
                     }else{
-                        if(i == 53){
+                        if(i == 53)
                             continue;
-                        }
                         i++; // para cada zero encontrado
                         }
                 j++;
             }
+            printf("%d",((ptr->info)->infoNum));
+            passoHuffman(&ptr); // ira construir a árvore
 
-            passoHuffman(ptr); // ira construir a árvore
+            
 
             break;
 
